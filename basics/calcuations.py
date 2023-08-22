7ada5420018cf45166c7bcfe02dcf26f04a54111
Ryan Kyle <ryandkyle@live.com>
2023-08-22 15:43:26 -0700

    Simple printing and calculations

---
+++ basics/calcuations.py
@@ -0,0 +1,6 @@
+calculation_days_to_seconds = 24 * 60 * 60
+
+print(f"1   day  is {1 * calculation_days_to_seconds} seconds")
+print(f"7   days is {7 * calculation_days_to_seconds} seconds")
+print(f"31  days is {31 * calculation_days_to_seconds} seconds")
+print(f"365 days is {365 * calculation_days_to_seconds} seconds")
--- basics/print.py
+++ basics/print.py
@@ -1,1 +1,7 @@
 print(1)
+
+print("Hello World")
+
+james = "James"
+print("Hello, " + james)
+print(f"Hello, {james}!")
